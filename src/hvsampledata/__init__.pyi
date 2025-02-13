f7fbedc2d077731c97503c30292ffac79a88a047
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-02-13 15:25:48 +0100

    enh: Make `engine` an argument instead of keyword argument (#17)

--- src/hvsampledata/__init__.pyi
+++ src/hvsampledata/__init__.pyi
@@ -16,84 +16,98 @@ __all__: tuple[str, ...]
 @overload
 def penguins(
     engine: None = None,
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[False] = False,
 ) -> pd.DataFrame | pl.DataFrame: ...
 @overload
 def penguins(
     engine: None = None,
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[True] = True,
 ) -> pl.LazyFrame | dd.DataFrame: ...
 @overload
 def penguins(
     engine: Literal["pandas"] = "pandas",
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[False] = False,
 ) -> pd.DataFrame: ...
 @overload
 def penguins(
     engine: Literal["polars"] = "polars",
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[False] = False,
 ) -> pl.DataFrame: ...
 @overload
 def penguins(
     engine: Literal["polars"] = "polars",
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[True] = True,
 ) -> pl.LazyFrame: ...
 @overload
 def penguins(
     engine: Literal["dask"] = "dask",
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[True] = True,
 ) -> dd.DataFrame: ...
 @overload
 def large_time_series(
     engine: None = None,
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[False] = False,
 ) -> pd.DataFrame | pl.DataFrame: ...
 @overload
 def large_time_series(
     engine: None = None,
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[True] = True,
 ) -> pl.LazyFrame | dd.DataFrame: ...
 @overload
 def large_time_series(
     engine: Literal["pandas"] = "pandas",
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[False] = False,
 ) -> pd.DataFrame: ...
 @overload
 def large_time_series(
     engine: Literal["polars"] = "polars",
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[False] = False,
 ) -> pl.DataFrame: ...
 @overload
 def large_time_series(
     engine: Literal["polars"] = "polars",
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[True] = True,
 ) -> pl.LazyFrame: ...
 @overload
 def large_time_series(
     engine: Literal["dask"] = "dask",
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     lazy: Literal[True] = True,
 ) -> dd.DataFrame: ...
 @overload
 def airplane(
     engine: None = None,
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     # lazy: Literal[False] = False,
 ) -> xr.Dataset: ...
 @overload
 def airplane(
     engine: Literal["dataset"] = "dataset",
+    *,
     engine_kwargs: dict[str, Any] | None = None,
     # lazy: Literal[False] = False,
 ) -> xr.Dataset: ...
