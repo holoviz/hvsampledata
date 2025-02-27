5972f61f37d0734d4312548cd922b8668be6a2b6
maximlt <mliquet@anaconda.com>
2025-02-27 11:54:25 +0100

    remove data citations as it's too long

--- src/hvsampledata/__init__.py
+++ src/hvsampledata/__init__.py
@@ -80,23 +80,6 @@ def penguins(
     Gorman KB, Williams TD, Fraser WR (2014). Ecological sexual dimorphism and
     environmental variability within a community of Antarctic penguins (genus
     Pygoscelis). PLoS ONE 9(3):e90081. https://doi.org/10.1371/journal.pone.0090081
-
-    Data citations:
-    - Adélie penguins: Palmer Station Antarctica LTER and K. Gorman, 2020.
-    Structural size measurements and isotopic signatures of foraging among adult male
-    and female Adélie penguins (Pygoscelis adeliae) nesting along the Palmer Archipelago
-    near Palmer Station, 2007-2009 ver 5. Environmental Data Initiative.
-    https://doi.org/10.6073/pasta/98b16d7d563f265cb52372c8ca99e60f (Accessed 2020-06-08).
-    - Gentoo penguins: Palmer Station Antarctica LTER and K. Gorman, 2020.
-    Structural size measurements and isotopic signatures of foraging among adult male
-    and female Gentoo penguin (Pygoscelis papua) nesting along the Palmer Archipelago
-    near Palmer Station, 2007-2009 ver 5. Environmental Data Initiative.
-    https://doi.org/10.6073/pasta/7fca67fb28d56ee2ffa3d9370ebda689 (Accessed 2020-06-08).
-    - Chinstrap penguins: Palmer Station Antarctica LTER and K. Gorman, 2020.
-    Structural size measurements and isotopic signatures of foraging among adult male
-    and female Chinstrap penguin (Pygoscelis antarcticus) nesting along the Palmer Archipelago
-    near Palmer Station, 2007-2009 ver 6. Environmental Data Initiative.
-    https://doi.org/10.6073/pasta/c14dfcfada8ea13a17536e73eb6fbe9e (Accessed 2020-06-08).
     """
     if engine in ["pandas", "dask"]:
         dtype = {
