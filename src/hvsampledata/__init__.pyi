661b1b4799d7e411684ce621ccee2fbbbe9c37c6
Isaiah Akorita <akoritaisaiah@gmail.com>
2025-07-10 22:23:53 +0100

    generate typehints

--- src/hvsampledata/__init__.pyi
+++ src/hvsampledata/__init__.pyi
@@ -126,6 +126,34 @@ def stocks(
     lazy: Literal[True] = True,
 ) -> dd.DataFrame: ...
 @overload
+def world_countries(
+    engine: Literal["pandas"],
+    *,
+    engine_kwargs: dict[str, Any] | None = None,
+    lazy: Literal[False] = False,
+) -> pd.DataFrame: ...
+@overload
+def world_countries(
+    engine: Literal["polars"],
+    *,
+    engine_kwargs: dict[str, Any] | None = None,
+    lazy: Literal[False] = False,
+) -> pl.DataFrame: ...
+@overload
+def world_countries(
+    engine: Literal["polars"],
+    *,
+    engine_kwargs: dict[str, Any] | None = None,
+    lazy: Literal[True] = True,
+) -> pl.LazyFrame: ...
+@overload
+def world_countries(
+    engine: Literal["dask"],
+    *,
+    engine_kwargs: dict[str, Any] | None = None,
+    lazy: Literal[True] = True,
+) -> dd.DataFrame: ...
+@overload
 def air_temperature(
     engine: Literal["xarray"],
     *,
